f9435180948e78aac2c30aa16a985d400fdb37ef
Charlie <not.an.email@gmail.com>
2023-01-23 22:38:57 +0000

    Whole game working

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!!!");
+        Application.Quit();
+    }
+}
